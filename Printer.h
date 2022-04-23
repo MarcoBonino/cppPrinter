32217049044e550bf458ab05687fc4eb9e7ad308
MarcoBonino <marco.bonino.91@gmail.com>
2022-04-23 16:25:36 +0200

    Printer for all SequenceContainer types
    - Compile with c++20.
    - Add c++20 concepts.
    - Add printer for array, deque, forward_list and list.

--- Printer.h
+++ Printer.h
@@ -1,11 +1,56 @@
 #pragma once
 
+#include <array>
+#include <concepts>
+#include <deque>
+#include <forward_list>
 #include <iostream>
+#include <list>
 #include <utility>
 #include <vector>
 
+template <typename, template <typename ...> typename>
+inline constexpr bool is_specialization = false;
+template <template <typename ...> typename T, typename ... ARGS>
+inline constexpr bool is_specialization<T<ARGS ...>, T> = true;
+
 template <typename T>
-std::ostream& operator<<(std::ostream& os, std::vector<T> v)
+concept isDeque = is_specialization<T, std::deque>;
+
+template <typename T>
+concept isForwardList = is_specialization<T, std::forward_list>;
+
+template <typename T>
+concept isList = is_specialization<T, std::list>;
+
+template <typename T>
+concept isVector = is_specialization<T, std::vector>;
+
+static_assert(isVector<std::vector<int>> and !isList<std::vector<int>>);
+static_assert(!isVector<std::list<int>> and isList<std::list<int>>);
+
+template <typename TYPE, template<typename, size_t> typename T, size_t N>
+inline constexpr bool is_specialization_array = false;
+
+template <template<typename, size_t> typename T, typename TYPE, size_t N>
+inline constexpr bool is_specialization_array<T<TYPE, N>, T, N> = true;
+
+template <typename>
+struct array_size;
+
+template <typename T, size_t N>
+struct array_size<std::array<T, N>>
+{
+    static const size_t value = N;
+};
+
+template <typename T>
+concept isArray = is_specialization_array<T, std::array, array_size<T>::value>;
+
+
+template <typename T>
+requires isArray<T> or isDeque<T> or isForwardList<T> or isList<T> or isVector<T>
+std::ostream& operator<<(std::ostream& os, T v)
 {
     os << "[";
     if (!v.empty())
--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <string>
-#include <utility>
-#include <vector>
 #include "Printer.h"
 
 int main(int argc, const char * argv[])
@@ -9,11 +6,24 @@ int main(int argc, const char * argv[])
     std::cout << "Hello, World!\n";
 
     std::vector<std::vector<std::string>> v1{{"first", "second", "third"}, {"a string", "another"}, {}, {"abc"}};
-    std::cout << v1 << std::endl;
+    std::cout << "v1 = " << v1 << std::endl;
 
     std::pair<int, std::string> p1('d', "five");
     std::pair<double, std::vector<int>> p2(3.14, {-2, 3, 7, -6});
-    std::cout << p1 << std::endl;
-    std::cout << p2 << std::endl;
+    std::cout << "p1 = " << p1 << std::endl;
+    std::cout << "p2 = " << p2 << std::endl;
+
+    std::array<std::string, 3> testArray{"first string from test array", "second", "third"};
+    std::cout << "testArray = " << testArray << std::endl;
+
+    std::list<double> myList{2.4, 1, 0, 8.8, 12.12345};
+    std::cout << "myList = " << myList << std::endl;
+
+    std::forward_list<int> fwList{0, 1, 2, 3, 4, 5, 6};
+    std::cout << "fwList = " << fwList << std::endl;
+
+    std::deque<int> dq(fwList.begin(), fwList.end());
+    std::cout << "dq = " << dq << std::endl;
+
     return 0;
 }
